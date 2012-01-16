571fedb97e797779cfe0ecb77180382022c55cc1
gsm <gsm1022@163.com>
2012-01-16 11:00:03 +0800

    初始化blog

---
+++ blog/app/controllers/home_controller.rb
@@ -0,0 +1,23 @@
+class HomeController < ApplicationController
+   #before_filter :authenticate_user! , :except => [:show]     #没有登录直接跳到登录界面
+  def index
+     @posts = Post.order("created_at desc").page(params[:page]).per(4)
+      tag_cloud #加入标签云
+     respond_to do |format|
+     format.html # index.html.erb
+    end
+  end
+
+    def tag_cloud
+    @tags = Post.tag_counts_on(:tags)
+  end
+
+  def tag
+       @posts= Post.tagged_with(params[:id])
+  end
+
+ def category
+   @posts=Post.find_by_category(params[:id])
+   render :action => new
+ end
+end
